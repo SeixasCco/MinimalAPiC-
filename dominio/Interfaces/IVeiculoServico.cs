97d2886296737ce71742bd1beccd148887fd10fb
SeixasCco <raulalves4@gmail.com>
2024-01-27 17:45:31 -0300

    configurando modelo de veiculos

---
+++ dominio/Interfaces/IVeiculoServico.cs
@@ -0,0 +1,17 @@
+using minimal_api.dominio.dto;
+using minimal_api.dominio.entidades;
+
+namespace minimal_api.dominio.Interfaces;
+
+public interface IVeiculoServico
+{
+    List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null);
+    Veiculo? BuscaPorId(int id);
+
+    void Incluir(Veiculo veiculo);
+
+    void Atualizar(Veiculo veiculo);
+
+    void Apagar(Veiculo veiculo);
+}
+
